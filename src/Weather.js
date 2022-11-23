7359f052d96237837a403eed51acfb08c09b78b6
Irene Tamayo <irenetamayo90@gmail.com>
2022-11-24 00:14:45 +0900

    Added axios integration so it shows the weather of a city

---
+++ src/Weather.js
@@ -0,0 +1,27 @@
+import React from "react";
+import axios from "axios";
+import { Puff } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+  let apiKey = "fe028dd951e1dd63d22f0a02e9c65071";
+  let units = "metric";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+  axios.get(url).then(handleResponse);
+  return (
+    <Puff
+      height="80"
+      width="80"
+      radisu={1}
+      color="#4fa94d"
+      ariaLabel="puff-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />
+  );
+}
