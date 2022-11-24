26aaccc2ede66853b659a8bd0c1a9ef8a5cdd990
Irene Tamayo <irenetamayo90@gmail.com>
2022-11-25 00:36:42 +0900

    Added background and change icon

---
+++ src/Days.js
@@ -0,0 +1,78 @@
+import React from "react";
+import ReactAnimatedWeather from "react-animated-weather";
+
+import "./App.css";
+export default function Days() {
+  return (
+    <div className="days">
+      <div className="row">
+        <div className="col-sm-2 next-day-1">
+          <div className="card day-card">
+            <div className="card-body">
+              <h5 className="card-title">Thursday</h5>
+              <div className="weather-icons">
+                <img src="https://img.icons8.com/office/60/000000/clouds.png" />
+              </div>
+              <p className="card-text temp">33°C / 27°C </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-2 next-day-1">
+          <div className="card day-card">
+            <div className="card-body">
+              <h5 className="card-title">Thursday</h5>
+              <div className="weather-icons">
+                <img src="https://img.icons8.com/office/60/000000/clouds.png" />
+              </div>
+              <p className="card-text temp">33°C / 27°C </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-2 next-day-1">
+          <div className="card day-card">
+            <div className="card-body">
+              <h5 className="card-title">Thursday</h5>
+              <div className="weather-icons">
+                <img src="https://img.icons8.com/office/60/000000/clouds.png" />
+              </div>
+              <p className="card-text temp">33°C / 27°C </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-2 next-day-1">
+          <div className="card day-card">
+            <div className="card-body">
+              <h5 className="card-title">Thursday</h5>
+              <div className="weather-icons">
+                <img src="https://img.icons8.com/office/60/000000/clouds.png" />
+              </div>
+              <p className="card-text temp">33°C / 27°C </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-2 next-day-1">
+          <div className="card day-card">
+            <div className="card-body">
+              <h5 className="card-title">Thursday</h5>
+              <div className="weather-icons">
+                <img src="https://img.icons8.com/office/60/000000/clouds.png" />
+              </div>
+              <p className="card-text temp">33°C / 27°C </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-2 next-day-1">
+          <div className="card day-card">
+            <div className="card-body">
+              <h5 className="card-title">Thursday</h5>
+              <div className="weather-icons">
+                <img src="https://img.icons8.com/office/60/000000/clouds.png" />
+              </div>
+              <p className="card-text temp">33°C / 27°C </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Temperature.js
+++ src/Temperature.js
@@ -1,18 +1,21 @@
 import React from "react";
+import ReactAnimatedWeather from "react-animated-weather";
 
 import "./App.css";
 
 export default function Temperature() {
   let weatherData = {
-    description: "broken clouds",
+    description: "cloudy",
   };
 
   return (
     <div className="col text-center">
       <div className="today-weather">
-        <img
-          src="https://img.icons8.com/office/150/000000/clouds.png"
-          alt="Cloudy"
+        <ReactAnimatedWeather
+          icon="CLOUDY"
+          color="#1ab2a8"
+          size={150}
+          animate={true}
         />
       </div>
       <div>
